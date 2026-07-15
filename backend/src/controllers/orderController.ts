fa523d35e8afa5e8717f8920cffb0b30bb905b30
vinodpr1 <vinodpr737947@gmail.com>
2026-07-16 01:22:44 +0530

    order

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -1,7 +1,35 @@
+import orderService from "../services/orderService";
+import type { Request, Response } from "express";
+import { exchangeSchema } from "../types/exchange-schema";
+import { validationErrors } from "../utils/validationErrors";
+import { createOrderEngone } from "../utils/engine";
+
+const getUser = (req: any) =>{
+    if(!req.user){
+        throw new Error("Missing authenticated user");
+    }
+    return req.user;
+}
 export default class orderController {
-    static getOrder = async (req:any, res:any) => {
+    static getOrder = async (req:Request, res:Response): Promise<void> => {
         try {
-            return res.json({Message: "Order"});
+            const parsedBody = exchangeSchema.safeParse(req.body);
+            if(!parsedBody.success){
+                validationErrors(res, parsedBody.error);
+                return;
+            }
+            const user = getUser(req);
+            const { type, price, qty, side, symbol } = parsedBody.data;
+
+        const engineResponse = await createOrderEngone("create_order", {
+            type,
+            side,
+            symbol,
+            price: type === "limit" ? price : null,
+            qty,
+            userid: user.id
+        })
+        res.status(engineResponse.ok ? 200 : 400).json(engineResponse.ok ? engineResponse.data : {error: engineResponse.error});
         } catch (error) {
             res.json({ message: "Internal server error" });
         }
